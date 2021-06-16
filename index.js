fc2e2237da704bf927963a170f7d287b589bccb5
WHeirstrate <56075460+WHeirstrate@users.noreply.github.com>
2021-06-16 14:46:28 +0200

    optimisation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const notifyForm = document.getElementById("notify-form");
-const contentHead = document.getElementById("content-head");
+const responseEl = document.getElementById("response-el");
 
 const checkSubmit = (email) => {
     if (sessionStorage.getItem("submit")) {
@@ -17,16 +17,11 @@ const checkSubmit = (email) => {
             }).then(
                 result => {
                     notifyForm.style.display = 'none';
-                    contentHead.innerHTML = "SEE YOU THEN!";
-                    particles();
+                    contentHead.innerHTML = "TOT SNEL!";
                 },
                 error => {
-
-                    //errortext
                     document.getElementById('notify').setAttribute('placeholder', 'Error, please try again!');
-                    //add red color
-                    document.getElementById('notify').classList.add('errorplaceholder');
-                    //clear input
+                    document.getElementById('notify').classList.add('error');
                     document.getElementById('notify').value = '';
 
                 }
@@ -45,8 +40,10 @@ notifyForm.addEventListener("submit", (e) => {
     checkSubmit(email);
 });
 
-const countDownDate = new Date("Jun 25, 2021 18:00:00").getTime();
 
+// REF: https://www.w3schools.com/howto/howto_js_countdown.asp
+
+const countDownDate = new Date("Jun 25, 2021 18:00:00").getTime();
 // Update the count down every 1 second
 setInterval(function () {
     // Get today's date and time
@@ -58,80 +55,6 @@ setInterval(function () {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    // Output the result in an element with id="demo"
     document.getElementById("countdown").innerHTML = days + "d " + hours + "h " +
         minutes + "m " + seconds + "s ";
 })
-
-function particles() {
-
-    tsParticles.load("tsparticles", {
-        fpsLimit: 60,
-        particles: {
-            number: {
-                value: 1000
-            },
-            color: {
-                value: ["#E73C3E", "#FBE122", "#489FDF"]
-            },
-            shape: {
-                type: "confetti",
-                options: {
-                    confetti: {
-                        type: ["circle", "square"]
-                    }
-                }
-            },
-            opacity: {
-                value: 3,
-                animation: {
-                    enable: true,
-                    minimumValue: 0,
-                    speed: 2,
-                    startValue: "max",
-                    destroy: "min"
-                }
-            },
-            size: {
-                value: 7,
-                random: {
-                    enable: true,
-                    minimumValue: 3
-                }
-            },
-            links: {
-                enable: false
-            },
-            life: {
-                duration: {
-                    sync: true,
-                    value: 15
-                },
-                count: 1
-            },
-            move: {
-                enable: true,
-                gravity: {
-                    enable: true,
-                    acceleration: 20
-                },
-                speed: 20,
-                decay: 0.1,
-                direction: "none",
-                random: false,
-                straight: false,
-                outModes: {
-                    default: "destroy",
-                    top: "none"
-                }
-            }
-        },
-        interactivity: {
-            detectsOn: "window",
-            events: {
-                resize: true
-            }
-        },
-        detectRetina: true,
-    });
-}
