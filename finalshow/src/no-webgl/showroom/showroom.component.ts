66ffa65c4574c93bf6cf6a59e18b1091399d098c
WHeirstrate <56075460+WHeirstrate@users.noreply.github.com>
2021-06-25 13:47:31 +0200

    rewrote fetch

--- finalshow/src/no-webgl/showroom/showroom.component.ts
+++ finalshow/src/no-webgl/showroom/showroom.component.ts
@@ -15,60 +15,60 @@ export class ShowroomComponent implements OnInit {
       window.location.href = '/hub';
     })
   }
-
   async fetchNominees(): Promise<Response> {
     const req = await fetch('https://finalshowcase.herokuapp.com/admin/get-nominations');
     return await req.json();
   }
 
+  fetchProjects(): Promise<Response>{
+    return fetch("https://finalshowcase.herokuapp.com/final-work/get-all").then((req: any) => {
+      return req.json().then((data: any) => {
+        return data;
+      });
+    });
+  }
+
+  async fetchSingleProject(project: String): Promise<Response>{
+    const req = await fetch(`https://finalshowcase.herokuapp.com/final-work/search-name/${project}`);
+    return await req.json();
+  }
+
+  clusterMenu(): String {
+    const buttons = document.getElementsByClassName('bottom-menu-cluster');
+    let cluster = "web";
+    document.querySelector(`a.${cluster}`).classList.add("active");
+    for (let button of buttons as Array) {
+      button.addEventListener("click", () => {
+        cluster = button.classList[0];
+        button.classList.add("active");
+        const children = button.parentNode.children;
+        for (let child of children as array) {
+          if(child.classList[0] != cluster) {
+            child.classList.remove("active");
+          }
+        }
+        //getapi(api_url);
+      });
+    }
+    return cluster;
+  }
+
   ngOnInit() {
     this.goTo2D();
-    var menubuttons = document.querySelectorAll("button");
-    var menucluster = "web";
-    menubuttons.forEach(button => {
-      button.addEventListener("click", function(){
-          menucluster = button.classList[0];
-          console.log(menucluster);
-          localStorage.setItem("cluster", menucluster);
-          window.location.href = "/no-webgl"
-      })
-    })
-
+    let cluster = this.clusterMenu();
+    this.fetchProjects().then((data: any) => {
+      displayData(data);
+    });
   
     //API call
 
-    const api_url = "https://finalshowcase.herokuapp.com/final-work/get-all";
-
-    var buttons = document.querySelectorAll(".bottom-menu a");
-    var cluster = localStorage.getItem("cluster")||"web";
-    document.querySelector(`a.${cluster}`).classList.add("active");
-    buttons.forEach(button => {
-        button.addEventListener("click", function(){
-            cluster = button.classList[0];
-            button.classList.add("active");
-            var children = [].slice.call(button.parentNode.children);
-            children.forEach(child => {
-                if(child.classList[0] != cluster) {
-                    child.classList.remove("active");
-                }
-            });
-            getapi(api_url);
-        })
-    });
-
-    async function getapi(url) {
-        const response = await fetch(url);
-        const data = await response.json();
-        displayData(data);
-    }
-
-    getapi(api_url);
+    //getapi(api_url);
 
     //Na API Call alle projecten inladen, rangschikken per cluster en deze printen adhv keuze gebruiker
 
     function displayData(data) {
       let nomineesID: String[] = [];
-      fetch('http://193.191.183.48:3000/admin/get-nominations').then((clusters: any) => {
+      fetch('https://finalshowcase.herokuapp.com/final-work/get-all').then((clusters: any) => {
         for (const [key, value] of Object.entries(clusters)) {
           //@ts-ignore
           for (let project of value) {
